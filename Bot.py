a6f5a481788dd9f088d895b0ddaf4a60a95fa9de
jyotikihla2050-collab <jyotikihla2050@gmail.com>
2026-06-03 19:31:37 +0530

    Change Telegram bot token
    Updated the Telegram bot token for authentication.

--- Bot.py
+++ Bot.py
@@ -4,7 +4,7 @@ from threading import Thread
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes, Application, CommandHandler
 from telegram import Update
 
-TOKEN = '8835968464:AAEgKB1vwE2S9cy2ziv5VT6jII1iNuZuxQ'
+TOKEN = '8835968464:AAE1VshLpK1WZUwnXeh2AqkIZwADv01fkGg'
 YOUR_CHAT_ID = 5306025504
 
 app = Flask(__name__)
